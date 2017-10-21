b4a1026581984538d4da51641199ecb3be9ab852
Brian Cottingham <spiffytech@gmail.com>
2017-10-21 10:55:19 -0400

    Display playlist in a pretty way

--- src/scenes/playlist/playlist.ts
+++ src/scenes/playlist/playlist.ts
@@ -1,12 +1,19 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
 
+import Episode from '../../components/episode/episode';
+
 @Component({
   template: require('./playlist.html'),
   props: {
-  }
+  },
+  components: {Episode}
 })
 export default class Playlist extends Vue {
+  get feeds() {
+    return this.$store.state.feeds;
+  }
+
   get episodes() {
     return this.$store.getters.playlist;
   }
