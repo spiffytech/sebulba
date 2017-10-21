1a4994bc1b14d847051f59cdcf8411c9ef5fe6e3
Brian Cottingham <spiffytech@gmail.com>
2017-10-21 11:29:31 -0400

    Wrap playlist under tab bar

--- src/scenes/playlist/playlist.ts
+++ src/scenes/playlist/playlist.ts
@@ -2,12 +2,13 @@ import Vue from 'vue';
 import Component from 'vue-class-component';
 
 import Episode from '../../components/episode/episode';
+import AppNav from '../../components/appnav/appnav';
 
 @Component({
   template: require('./playlist.html'),
   props: {
   },
-  components: {Episode}
+  components: {AppNav, Episode}
 })
 export default class Playlist extends Vue {
   get feeds() {
